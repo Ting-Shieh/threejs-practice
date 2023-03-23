f363117f9f5d65287639c5860b7aff018be8f17b
Ting-Shieh <zsting29@gmail.com>
2023-03-24 07:41:01 +0800

    add initControl =>深度材質

--- controls/index.js
+++ controls/index.js
@@ -52,8 +52,18 @@ const basicType = {
     setValue: (item, value) => item.wireframe = value,
   },
   visible: {
-    getValue: item => item.visible,
+    getValue: (item) => item.visible,
     setValue: (item, value) => item.visible = value,
+  },
+  cameraNear: {
+    extends: [0, 50],
+    getValue: (item, camera) => camera.near,
+    setValue: (item, value, camera) => camera.near = value,
+  },
+  cameraFar: {
+    extends: [50, 1000],
+    getValue: (item, camera) => camera.far,
+    setValue: (item, value, camera) => camera.far = value,
   }
 }
 const itemType = {
@@ -63,10 +73,12 @@ const itemType = {
   PointLight: ['color', 'intensity', 'distance'],
   DirectionalLight: ['color', 'intensity'],
   HemisphereLight: [ 'color', 'groundColor', 'intensity'],
-  MeshBasicMaterial: ['color', 'opacity', 'transparent', 'wireframe', 'visible']
+  MeshBasicMaterial: ['color', 'opacity', 'transparent', 'wireframe', 'visible'],
+  MeshDepthMaterial: ['opacity', 'wireframe', 'cameraNear', 'cameraFar']
 }
-function initControls(item) {
+function initControls(item, camera) {
   console.log(item)
+  console.log(camera)
   // 創建ＧＵＩ實例
   const gui = new dat.GUI()
   // 獲取物件可配置項目清單
@@ -79,12 +91,12 @@ function initControls(item) {
     const child = basicType[typeList[i]]
 
     if (child) { 
-      controls[typeList[i]] = child.getValue(item)
+      controls[typeList[i]] = child.getValue(item, camera)
       // 是否有 extends
       const childExtends = child.extends || []
 
       gui[child.method || 'add'](controls, typeList[i], ...childExtends).onChange((val) => {
-        child.setValue(item, val)
+        child.setValue(item, val, camera)
       })
      }
   }
