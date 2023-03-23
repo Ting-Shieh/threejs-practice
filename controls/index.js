7b3992220783d06dbdc5aa3bfcd4cdc47cfa5067
Ting-Shieh <zsting29@gmail.com>
2023-03-24 00:51:06 +0800

    add initControl =>半球光 基礎材質

--- controls/index.js
+++ controls/index.js
@@ -27,12 +27,43 @@ const basicType = {
     extends: [0, 20],
     getValue: item => item.exponent,
     setValue: (item, value) => item.exponent = +value,
+  },
+  groundColor: {
+    method: 'addColor',
+    getValue: item => item.groundColor.getStyle(),
+    setValue: (item, value) => item.groundColor.setStyle(value),
+  },
+  wireframeLinewidth: {
+    extends: [0, 20],
+    getValue: item => item.wireframeLinewidth,
+    setValue: (item, value) => item.wireframeLinewidth = +value,
+  },
+  opacity: {
+    extends: [0, 1],
+    getValue: item => item.opacity,
+    setValue: (item, value) => item.opacity = +value,
+  },
+  transparent: {
+    getValue: item => item.transparent,
+    setValue: (item, value) => item.transparent = value,
+  },
+  wireframe: {
+    getValue: item => item.wireframe,
+    setValue: (item, value) => item.wireframe = value,
+  },
+  visible: {
+    getValue: item => item.visible,
+    setValue: (item, value) => item.visible = value,
   }
 }
 const itemType = {
+  extends: [0, 20],
   SpotLight: ['color', 'intensity', 'distance', 'angle', 'exponent'],
   AmbientLight: ['color'],
-  PointLight: ['color', 'intensity', 'distance']
+  PointLight: ['color', 'intensity', 'distance'],
+  DirectionalLight: ['color', 'intensity'],
+  HemisphereLight: [ 'color', 'groundColor', 'intensity'],
+  MeshBasicMaterial: ['color', 'opacity', 'transparent', 'wireframe', 'visible']
 }
 function initControls(item) {
   console.log(item)
@@ -42,7 +73,7 @@ function initControls(item) {
   const typeList = itemType[item.type]
   // 可配置項目
   const controls = {}
-  if (!typeList.length) { return }
+  if (!typeList || !typeList.length) { return }
 
   for (let i = 0; i < typeList.length; i++) {
     const child = basicType[typeList[i]]
